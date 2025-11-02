d4719f5fe8ad4a740681c202da6912400d834332
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-02 18:40:57 +0200

    feat: configuring database connection

--- bloom_art/bloom_art/settings.py
+++ bloom_art/bloom_art/settings.py
@@ -68,14 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bloom_art.wsgi.application'
 
+import os
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'mydb'),
+        'USER': os.getenv('DB_USER', 'myuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
+        'HOST': "db",
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
