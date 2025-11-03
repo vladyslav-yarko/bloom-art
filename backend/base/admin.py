49c37cb94308eb7d94b41a15fde5411ef30831e4
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-04 00:28:57 +0200

    feat: add picture overview to admin panel

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from django import forms
+from django.core.exceptions import ValidationError
+import base64
 
 from .models import Perfume
 from .redis_manager import redis_manager
@@ -31,7 +33,7 @@ class PerfumeForm(forms.ModelForm):
                 image_hex = image_bytes.hex()
                 redis_manager.add_photo('perfume', instance.id, image_hex)
             except Exception as e:
-                raise ValidationError(f"Failed to save picture")
+                raise ValidationError(f"Failed to save picture: {str(e)}")
         return instance
 
 
@@ -41,7 +43,7 @@ class PerfumeAdmin(admin.ModelAdmin):
 
     list_display = (
         'title', 'price', 'currency', 'available',
-        'type', 'volume', 'sex', 'display_notes'
+        'type', 'volume', 'sex', 'display_notes', 'picture_preview'
     )
     list_editable = ('price', 'available')
     list_filter = ('currency', 'type', 'volume', 'sex', 'first_notes', 'perfume_heart', 'last_notes')
@@ -50,7 +52,7 @@ class PerfumeAdmin(admin.ModelAdmin):
     list_per_page = 30
 
     fieldsets = (
-        ('Main Info', {'fields': ('title', 'type', 'description', 'sex')}),
+        ('Main Info', {'fields': ('title', 'type', 'description', 'sex', 'picture')}),
         ('Pricing & Stock', {'fields': ('price', 'currency', 'available')}),
         ('Perfume Details', {'fields': ('weight', 'volume')}),
         ('Fragrance Notes', {'fields': ('first_notes', 'perfume_heart', 'last_notes')}),
@@ -59,6 +61,17 @@ class PerfumeAdmin(admin.ModelAdmin):
     def display_notes(self, obj):
         return f"{obj.first_notes} → {obj.perfume_heart} → {obj.last_notes}"
     display_notes.short_description = "Notes Composition"
+
+
+    def picture_preview(self, obj):
+        image_hex = redis_manager.get_photo('perfume', obj.id)
+        if image_hex:
+            image_bytes = bytes.fromhex(image_hex)
+            data_uri = base64.b64encode(image_bytes).decode()
+            return f'<img src="data:image/png;base64,{data_uri}" width="50"/>'
+        return "-"
+    picture_preview.allow_tags = True
+    picture_preview.short_description = "Picture"
 
     actions = ['mark_as_unavailable', 'mark_as_available']
 
