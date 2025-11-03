76e11d370ac6ea64160dfa907334ca970d1ac219
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-04 00:04:41 +0200

    feat: add not null to Perfume ORM

--- backend/base/models.py
+++ backend/base/models.py
@@ -15,36 +15,42 @@ from .enums import (
 class Perfume(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
-    title = models.CharField(max_length=255)
-    price = models.IntegerField()
+    title = models.CharField(max_length=255, null=False)
+    price = models.IntegerField(null=False)
     currency = models.CharField(
         max_length=10,
-        choices=PriceCurrencyEnum.choices
+        choices=PriceCurrencyEnum.choices,
+        null=False
     )
-    available = models.IntegerField()
-    type = models.CharField(max_length=100)
-    weight = models.FloatField()
+    available = models.IntegerField(null=False)
+    type = models.CharField(max_length=100, null=False)
+    weight = models.FloatField(null=False)
     volume = models.CharField(
         max_length=100,
-        choices=VolumeMl.choices
+        choices=VolumeMl.choices,
+        null=False
     )
-    description = models.TextField()
+    description = models.TextField(null=False)
     first_notes = models.CharField(
         max_length=100,
-        choices=FirstNote.choices
+        choices=FirstNote.choices,
+        null=False
     )
     perfume_heart = models.CharField(
         max_length=100,
-        choices=HeartNote.choices
+        choices=HeartNote.choices,
+        null=False
     )
     last_notes = models.CharField(
         max_length=100,
-        choices=LastNote.choices
+        choices=LastNote.choices,
+        null=False
     )
     sex = models.CharField(
         max_length=100,
         choices=Sex.choices,
-        default=Sex.UNISEX
+        default=Sex.UNISEX,
+        null=False
     )
 
     class Meta:
