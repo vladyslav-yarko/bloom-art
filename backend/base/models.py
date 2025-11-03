5aead70d4da7b2c196e0714f60057e3ea4f23922
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 19:25:52 +0200

    feat: add Perfume orm model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,6 +1,31 @@
+import uuid
+
 from django.db import models
 
 
 class PriceCurrencyEnum(models.TextChoices):
     uah = 'uah'
     usd = 'usd'
+
+
+class Perfume(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    currency = models.CharField(
+        max_length=10,
+        choices=PriceCurrencyEnum.choices
+    )
+    available = models.IntegerField()
+    type = models.CharField(max_length=100)
+    weight = models.FloatField()
+    volume = models.FloatField()
+    description = models.TextField()
+    first_notes = models.CharField(max_length=255)
+    perfume_heart = models.CharField(max_length=255)
+    last_notes = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "perfumes"
+        managed = False
