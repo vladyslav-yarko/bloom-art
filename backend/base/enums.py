618f2e37e7bfc8803b91664b6ce1ee767378cd70
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 20:29:54 +0200

    feat: add VolumeMl

--- backend/base/enums.py
+++ backend/base/enums.py
@@ -4,3 +4,12 @@ from django.db import models
 class PriceCurrencyEnum(models.TextChoices):
     uah = 'uah'
     usd = 'usd'
+
+
+class VolumeMl(models.TextChoices):
+    ML_10  = 10
+    ML_20  = 20
+    ML_30  = 30
+    ML_50  = 50
+    ML_75  = 75
+    ML_100 = 100
