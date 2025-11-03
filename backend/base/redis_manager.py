ec0c9579f200b5b22a344cc7902c7c5afbb42b1d
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 23:47:54 +0200

    feat: add RedisManager

---
+++ backend/base/redis_manager.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+import redis
+
+from backend.config import settings
+
+
+class RedisManager:
+    REDIS_URL = settings.REDIS
+
+    def __init__(self):
+        self.engine = redis.from_url(self.REDIS_URL)
+
+    async def get_string_data(self, key: str) -> Union[None, str, dict, list]:
+        data = await self.engine.get(key)
+        return data.decode() if data else data
+
+    async def set_string_data(self, key: str, data: str, expired: int = 60) -> None:
+        await self.engine.set(key, data)
+        await self.engine.expire(key, expired)
+
+    async def ttl(self, key: str) -> int:
+        time = await self.engine.ttl(key)
+        return time
+
+    async def delete(self, key: str) -> None:
+        await self.engine.expire(key, 0)
+
+
+redis_manager = RedisManager()
