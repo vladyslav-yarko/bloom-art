3a300e80bdb32dda235e9664682c852ef3d9cdb0
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 23:52:48 +0200

    feat: add redis url to config settings

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -6,14 +6,16 @@ load_dotenv(find_dotenv())
 
 
 class Settings(BaseSettings):
-    
+
+    REDIS_URL: str
+
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: str
-    
+
     model_config = SettingsConfigDict(env_file='.env')
-    
-    
+
+
 settings = Settings()
