38c9b776cf46f8b7a29253b7d50b0fc7a322a57a
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 20:30:25 +0200

    feat: add FirstNote

--- backend/base/enums.py
+++ backend/base/enums.py
@@ -13,3 +13,63 @@ class VolumeMl(models.TextChoices):
     ML_50  = 50
     ML_75  = 75
     ML_100 = 100
+
+
+class FirstNote(models.TextChoices):
+    BERGAMOT = "bergamot"
+    LEMON = "lemon"
+    LIME = "lime"
+    GRAPEFRUIT = "grapefruit"
+    MANDARIN = "mandarin"
+    TANGERINE = "tangerine"
+    ORANGE_SWEET = "sweet orange"
+    ORANGE_BITTER = "bitter orange"
+    POMELO = "pomelo"
+    YUZU = "yuzu"
+    KUMQUAT = "kumquat"
+    CITRON = "citron"
+    PETITGRAIN = "petitgrain"
+
+    BLACKCURRANT_LEAF = "blackcurrant leaf"
+    GREEN_LEAF = "green leaf"
+    FRESH_GRASS = "fresh grass"
+    CUCUMBER = "cucumber"
+    TOMATO_LEAF = "tomato leaf"
+    BAMBOO = "bamboo"
+    SHISO = "shiso"
+    VERBENA = "verbena"
+    LEMONGRASS = "lemongrass"
+
+    SEA_SALT = "sea salt"
+    MARINE_BREEZE = "marine breeze"
+    OZONE = "ozone"
+    AQUATIC_NOTES = "aquatic notes"
+
+    PEPPERMINT = "peppermint"
+    MINT = "mint"
+    BASIL = "basil"
+    ROSEMARY = "rosemary"
+    THYME = "thyme"
+    TARRAGON = "tarragon"
+    CLARY_SAGE = "clary sage"
+    EUCALYPTUS = "eucalyptus"
+    JUNIPER_BERRY = "juniper berry"
+
+    GINGER = "ginger"
+    PINK_PEPPER = "pink pepper"
+    BLACK_PEPPER = "black pepper"
+    CARDAMOM = "cardamom"
+    CORIANDER = "coriander"
+    ANISE = "anise"
+    FENNEL = "fennel"
+
+    GREEN_APPLE = "green apple"
+    PEAR = "pear"
+    PINEAPPLE = "pineapple"
+    MELON = "melon"
+    WATERMELON = "watermelon"
+    LYCHEE = "lychee"
+    WHITE_GRAPE = "white grape"
+    KIWI = "kiwi"
+
+    ALDEHYDES = "aldehydes"
