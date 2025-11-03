bcce6662e63bcd753ce2682a7884131cd881c53c
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-03 21:02:43 +0200

    feat: add custom admin panel for django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "base"
 ]
+
+JET_SIDE_MENU_COMPACT = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django import forms
-from django.contrib import admin
 from django.contrib.auth.models import User, Group
-
+from django import forms
 from .models import Perfume
 
 
@@ -24,60 +22,26 @@ class PerfumeAdmin(admin.ModelAdmin):
     form = PerfumeForm
 
     list_display = (
-        'title',
-        'price',
-        'currency',
-        'available',
-        'type',
-        'volume',
-        'sex',
-        'display_notes',
+        'title', 'price', 'currency', 'available',
+        'type', 'volume', 'sex', 'display_notes'
     )
-
     list_editable = ('price', 'available')
-
-    list_filter = (
-        'currency',
-        'type',
-        'volume',
-        'first_notes',
-        'perfume_heart',
-        'last_notes',
-        'sex',
-    )
-
-    search_fields = (
-        'title',
-        'description',
-        'first_notes',
-        'perfume_heart',
-        'last_notes',
-    )
-
+    list_filter = ('currency', 'type', 'volume', 'sex', 'first_notes', 'perfume_heart', 'last_notes')
+    search_fields = ('title', 'description', 'first_notes', 'perfume_heart', 'last_notes')
     ordering = ('title',)
-
     list_per_page = 30
 
     fieldsets = (
-        ('Main Information', {
-            'fields': ('title', 'type', 'description', 'sex')
-        }),
-        ('Pricing and Stock', {
-            'fields': ('price', 'currency', 'available')
-        }),
-        ('Perfume Details', {
-            'fields': ('weight', 'volume')
-        }),
-        ('Fragrance Notes', {
-            'fields': ('first_notes', 'perfume_heart', 'last_notes')
-        }),
+        ('Main Info', {'fields': ('title', 'type', 'description', 'sex')}),
+        ('Pricing & Stock', {'fields': ('price', 'currency', 'available')}),
+        ('Perfume Details', {'fields': ('weight', 'volume')}),
+        ('Fragrance Notes', {'fields': ('first_notes', 'perfume_heart', 'last_notes')}),
     )
 
     def display_notes(self, obj):
         return f"{obj.first_notes} → {obj.perfume_heart} → {obj.last_notes}"
     display_notes.short_description = "Notes Composition"
 
-    # Add custom bulk actions
     actions = ['mark_as_unavailable', 'mark_as_available']
 
     def mark_as_unavailable(self, request, queryset):
