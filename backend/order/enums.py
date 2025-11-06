76dd9529e5f27e2424b45efbbb328d63530825ab
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:45:19 +0200

    feat: add CompanyPrefix enum to order app

--- backend/order/enums.py
+++ backend/order/enums.py
@@ -5,3 +5,9 @@ class Company(models.TextChoices):
     NOVA = 'nova', 'Nova'
     UKR = 'ukr', 'Ukr'
     MEEST = 'meest', 'Meest'
+
+
+class CompanyPrefix(models.TextChoices):
+    NOVA = 'nova', 'Nova'
+    UKR = 'ukr', 'Ukr'
+    MEEST = 'meest', 'Meest'
