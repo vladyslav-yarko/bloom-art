01a174cfb173778458b2ab696b5eccb4f36978af
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:46:51 +0200

    feat: add DeliveryCompany ORM to order app

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from .enums import Company, CompanyPrefix
+
+
+class DeliveryCompany(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=20, choices=Company.choices)
+    prefix = models.CharField(max_length=20, choices=CompanyPrefix.choices)
+
+    class Meta:
+        db_table = "delivery_companies"
+
+    def __str__(self):
+        return self.title
