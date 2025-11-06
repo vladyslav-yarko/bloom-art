689e3b3ca1139a740b20c7e98bcc1e3cbe9327c9
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:57:04 +0200

    feat: add CargoType enum to nova app

--- backend/nova/enums.py
+++ backend/nova/enums.py
@@ -4,3 +4,9 @@ from django.db import models
 class PayerType(models.TextChoices):
     SENDER = "Sender", "Sender"
     RECIPIENT = "Recipient", "Recipient"
+
+
+class CargoType(models.TextChoices):
+    CARGO = "Cargo", "Cargo"
+    PARCEL = "Parcel", "Parcel"
+    DOCUMENTS = "Documents", "Documents"
