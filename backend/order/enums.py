e14288a7de5575c400b7dd272e0f492f0b4107c4
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:50:11 +0200

    feat: add Status enum to order app

--- backend/order/enums.py
+++ backend/order/enums.py
@@ -11,3 +11,14 @@ class CompanyPrefix(models.TextChoices):
     NOVA = 'nova', 'Nova'
     UKR = 'ukr', 'Ukr'
     MEEST = 'meest', 'Meest'
+
+
+class Status(models.TextChoices):
+    AWAITING_ACCEPTANCE = "awaiting acceptance", "Awaiting Acceptance"
+    AWAITING_SHIPMENT = "awaiting shipment", "Awaiting Shipment"
+    IN_TRANSIT = "in transit", "In Transit"
+    ARRIVED = "arrived", "Arrived"
+    ACCEPTED = "accepted", "Accepted"
+    SUCCESS = "success", "Success"
+    RETURNED = "returned", "Returned"
+    DELETED = "deleted", "Deleted"
