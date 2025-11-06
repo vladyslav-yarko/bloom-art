ac4ad5a065233cba002ae40888cfadf4e2205e0a
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:52:41 +0200

    feat: add Order ORM to order app

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,8 +1,14 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
-from .enums import Company, CompanyPrefix
+from .enums import (
+    Company,
+    CompanyPrefix,
+    Payment,
+    Status
+)
 
 
 class DeliveryCompany(models.Model):
@@ -15,3 +21,33 @@ class DeliveryCompany(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    status = models.CharField(
+        max_length=50,
+        choices=Status.choices,
+        default=Status.AWAITING_ACCEPTANCE,
+    )
+    payment = models.CharField(
+        max_length=20,
+        choices=Payment.choices,
+        default=Payment.COD,
+    )
+    price = models.IntegerField()
+
+    delivery_company = models.ForeignKey(
+        'DeliveryCompany',
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+
+    delivery_id = models.UUIDField()
+    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "orders"
