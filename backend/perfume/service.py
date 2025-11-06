f26916d98024e02c20ec19859893875801de463e
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 23:51:26 +0200

    feat: add PerfumeService

---
+++ backend/perfume/service.py
@@ -0,0 +1,10 @@
+from utils.service import Service
+from utils.repository import Repository
+
+
+class PerfumeService(Service):
+    def __init__(
+        self,
+        perfume_repo: Repository
+    ):
+        self.repo = perfume_repo
