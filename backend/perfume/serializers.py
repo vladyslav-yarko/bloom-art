802fafde2a1920f7bdb4b7dcaca05dbc2a294501
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 22:24:26 +0200

    feat: add picture to perfume views

--- backend/perfume/serializers.py
+++ backend/perfume/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 
+from base.redis_manager import redis_manager
 from .models import Perfume
 
 
 class PerfumesSerializer(serializers.ModelSerializer):
-    picture = serializers.ImageField(required=True)
+    picture = serializers.SerializerMethodField()
     
     class Meta:
         model = Perfume
-        fields = ["id", "title", "price", "currency", "available", "type", "weight", "volume", "description", "first_notes", "perfume_heart", "last_notes", "sex"]
+        fields = ["id", "title", "price", "currency", "available", "type", "weight", "volume", "description", "first_notes", "perfume_heart", "last_notes", "sex", "picture"]
+
+    def get_picture(self, obj):
+        image_hex = redis_manager.get_photo('perfume', str(obj.id))
+        return image_hex
