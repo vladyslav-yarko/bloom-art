60e1bec3e987f4476836406f21a6cefb0af63c95
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 22:09:19 +0200

    feat: add perfume views

--- backend/perfume/views.py
+++ backend/perfume/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Perfume
+from .serializers import PerfumesSerializer
+
+
+class PerfumeAllView(APIView):
+    def get(self, request: HttpRequest):
+        perfumes = Perfume.objects.all()
+        serializer = PerfumesSerializer(perfumes, many=True)
+        return Response(serializer.data)
+
+
+class PerfumeOneView(APIView):
+    def get(self, request: HttpRequest, pk):
+        try:
+            perfume = Perfume.objects.get(pk=pk)
+        except Perfume.DoesNotExist:
+            return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PerfumesSerializer(perfume)
+        return Response(serializer.data)
