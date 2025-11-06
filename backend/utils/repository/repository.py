e596d80316fe31836c29093ad919a5cdc3f1c57d
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 22:54:16 +0200

    feat: add Repository abstract util class

---
+++ backend/utils/repository/repository.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class Repository(ABC):
+
+    @abstractmethod
+    async def get():
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_one():
+        raise NotImplementedError
+
+    # @abstractmethod
+    # async def get_count():
+    #     raise NotImplementedError
+
+    @abstractmethod
+    async def create_one():
+        raise NotImplementedError
+
+    # @abstractmethod
+    # async def update_one():
+    #     raise NotImplementedError
+
+    # @abstractmethod
+    # async def delete_one():
+    #     raise NotImplementedError
