9568e7f3b570dc9965a8c8ab4783b000017b0319
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-06 16:57:28 +0200

    feat: add ServiceType enum to nova app

--- backend/nova/enums.py
+++ backend/nova/enums.py
@@ -10,3 +10,10 @@ class CargoType(models.TextChoices):
     CARGO = "Cargo", "Cargo"
     PARCEL = "Parcel", "Parcel"
     DOCUMENTS = "Documents", "Documents"
+
+
+class ServiceType(models.TextChoices):
+    WAREHOUSE_WAREHOUSE = "WarehouseWarehouse", "Warehouse → Warehouse"
+    WAREHOUSE_DOORS = "WarehouseDoors", "Warehouse → Doors"
+    DOORS_WAREHOUSE = "DoorsWarehouse", "Doors → Warehouse"
+    DOORS_DOORS = "DoorsDoors", "Doors → Doors"
