70c4ffa0b446076ca2e49634c7f71286fdde0258
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-10 17:55:32 +0200

    feat: add nova order serializers

---
+++ backend/nova/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from order.enums import Source
+
+
+class LocalitySerializer(serializers.Serializer):
+    fullTitle = serializers.CharField()
+    title = serializers.CharField()
+    cityRef = serializers.UUIDField()
+    localityType = serializers.CharField()
+    localityTypeShorthand = serializers.CharField()
+    region = serializers.CharField()
+
+
+class LocalitiesPublicSerializer(serializers.Serializer):
+    data = LocalitySerializer(many=True)
+    source = serializers.ChoiceField(choices=Source.choices)
