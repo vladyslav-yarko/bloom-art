56bbb3a8146e5c37324653d61220b44b351b8b2b
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-10 17:14:25 +0200

    feat: add OrderItemRepository

--- backend/order/repository.py
+++ backend/order/repository.py
@@ -2,8 +2,12 @@ from typing import Optional
 
 from utils.repository import DjangoORMRepository
 
-from .models import Order
+from .models import Order, OrderItem
 
 
 class OrderRepository(DjangoORMRepository):
     model = Order
+
+
+class OrderItemRepository(DjangoORMRepository):
+    model = OrderItem
