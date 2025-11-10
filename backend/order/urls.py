1b50922938f64ed91ffe5e60452500a7f1c219ec
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-11 00:02:45 +0200

    feat: add GetOrderView

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.GetOrdersView.as_view(), name='orders-get-all')
+    path('', views.GetOrdersView.as_view(), name='orders-get-all'),
+    path('<uuid:id>/', views.GetOrderView.as_view(), name='orders-get-one')
 ]
--- backend/order/views.py
+++ backend/order/views.py
@@ -5,7 +5,8 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 from .serializers import (
-    OrdersPublicSerializer
+    OrdersPublicSerializer,
+    OrderPublicSerializer
 )
 from .service import OrderService
 from .repository import OrderRepository, OrderItemRepository, DeliveryCompanyRepository
@@ -40,4 +41,34 @@ class GetOrdersView(APIView):
         #         clean_data.append(item)
         #     data["data"] = clean_data
         serializer = OrdersPublicSerializer(data)
+        return Response(serializer.data)
+
+
+class GetOrderView(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'id',
+                openapi.IN_PATH,
+                description="Order ID",
+                type=openapi.TYPE_STRING,
+                format=openapi.FORMAT_UUID,
+                required=True,
+                example="8e1718f5-1972-11e5-add9-005056887b8d",
+            ),
+        ]
+    )
+    def get(self, request, id):
+        service = OrderService(
+            order_repo=OrderRepository,
+            order_item_repo=OrderItemRepository,
+            delivery_company_repo=DeliveryCompanyRepository
+        )
+        data = service.get_order(id)
+        if not data:
+            return Response(
+                {"detail": "Order by order id has not found"},
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+        serializer = OrderPublicSerializer(data)
         return Response(serializer.data)
