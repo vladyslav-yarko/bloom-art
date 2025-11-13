d1a285b5cf8db161b9d785b60e4217893066fc58
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-14 00:46:29 +0200

    feat: add NotFoundPage

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import NotFoundIcon from "@/ui/NotFoundIcon"
+import ButtonPrimary from "@/ui/ButtonPrimary"
+import LinkComponent from "@/ui/LinkComponent"
+
+
+export default function NotFoundPage() {
+
+	return (
+		<div>
+			<div className='flex justify-center items-center'>
+				<NotFoundIcon className='notFoundIcon' />
+				<h2>Page not found</h2>
+			</div>
+			<div className='flex justify-center items-center'>
+				<div className='notFoundButton'>
+					<ButtonPrimary>
+						<h2 className='w-full h-full'>
+							<LinkComponent title='Return to home' link='/' onClick={() => null} />
+						</h2>
+					</ButtonPrimary>
+				</div>
+			</div>
+		</div>
+	)
+}
