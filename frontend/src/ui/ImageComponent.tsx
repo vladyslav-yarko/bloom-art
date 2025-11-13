326529c7fcf9610fb45a5080918f015cd740ad63
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-14 00:27:04 +0200

    feat: add ImageComponent

---
+++ frontend/src/ui/ImageComponent.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+
+interface Props {
+    src: string,
+    alt: string,
+    className?: string
+}
+
+
+export default function ImageComponent({ src, alt, className="" }: Props) {
+
+    return (
+        <div className={`${className}`}>
+            <Image 
+                src={src}
+                alt={alt}
+                fill
+            />
+        </div>
+    )
+}
