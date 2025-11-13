40eb10dfdb9093749caeeedae586a210f2aebdcd
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-14 00:45:37 +0200

    feat: add LoadingPage

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingState from "@/ui/LoadingState"
+
+
+export default function LoadingPage() {
+	return (
+		<div className="loading">
+			<LoadingState className="loadingIcon"/>
+		</div>
+	)
+}
