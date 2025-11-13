13e903dfbfd8ba3ae624bb4db33415ff2368230a
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-13 20:38:07 +0200

    feat: add favicons

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,8 +4,22 @@ import type { Metadata } from "next";
 
 
 export const metadata: Metadata = {
-	title: 'Artstudia',
-	description: 'Artstudia',
+	title: 'a',
+	description: 'a',
+	icons: {
+		icon: [
+			{ url: '/favicon.ico' },
+			{ url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
+		other: [
+			{
+				rel: 'manifest',
+				url: '/site.webmanifest',
+			},
+		],
+	},
 }
 
 
