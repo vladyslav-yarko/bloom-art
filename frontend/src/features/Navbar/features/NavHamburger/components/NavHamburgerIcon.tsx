3574927da0e3a8fa1a2dfb08d082e282db33ddda
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-15 00:25:18 +0200

    feat: add NavHamburgerIcon

---
+++ frontend/src/features/Navbar/features/NavHamburger/components/NavHamburgerIcon.tsx
@@ -0,0 +1,27 @@
+import { SVGProps } from "react"
+
+
+interface Props extends SVGProps<SVGSVGElement> {
+	menuOpened?: boolean
+}
+
+
+export default function NavHamburgerIcon(props: Props) {
+	return (
+		<div className={`hamburgerIcon ${props.menuOpened ? "text-accent dark:text-accent-dark" : ""}`}>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='100%'
+				height='100%'
+				viewBox='0 0 512 512'
+				{...props}
+			>
+				{/* Icon from Game Icons by GameIcons - https://github.com/game-icons/icons/blob/master/license.txt */}
+				<path
+					fill='currentColor'
+					d='M32 96v64h448V96zm0 128v64h448v-64zm0 128v64h448v-64z'
+				/>
+			</svg>
+		</div>
+	)
+}
