ea5a71a749085863b52c2b110c7bd41d0c2f2a64
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-18 01:25:47 +0200

    feat: add blur

--- frontend/src/features/ProductModal/ProductModal.tsx
+++ frontend/src/features/ProductModal/ProductModal.tsx
@@ -32,8 +32,8 @@ export default function ProductModal() {
     }
 
     return (
-			<div className='modal' onClick={onClose}>
-				<div className='modal-content' onClick={e => e.stopPropagation()}>
+			<div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm' onClick={onClose}>
+				<div className='bg-background dark:bg-background-dark p-10 rounded-2xl' onClick={e => e.stopPropagation()}>
 					<button
 						className='modal-close'
 						onClick={onClose}
@@ -74,30 +74,26 @@ export default function ProductModal() {
 							)}
 						</div>
 						
-						<div className='modal-actions'>
-							{/* {product.available >= 1 ? (
-								<button
-									className='modal-btn modal-btn-primary'
-									onClick={handleAddToCart}
-								>
-									<svg
-										width='16'
-										height='16'
-										viewBox='0 0 24 24'
-										fill='currentColor'
-									>
-										<path d='M7 4V2C7 1.45 7.45 1 8 1H16C16.55 1 17 1.45 17 2V4H20C20.55 4 21 4.45 21 5S20.55 6 20 6H19V19C19 20.1 18.1 21 17 21H7C5.9 21 5 20.1 5 19V6H4C3.45 6 3 5.55 3 5S3.45 4 4 4H7ZM9 3V4H15V3H9ZM7 6V19H17V6H7Z' />
-									</svg>
-									Add to Cart
-								</button>
-							) : (
-								<button className='modal-btn modal-btn-secondary' disabled>
-									Out of Stock
-								</button>
-							)} */}
-
-							<button className='modal-btn modal-btn-secondary' onClick={onClose}>Close</button>
-						</div>
+                        {/* {product.available >= 1 ? (
+                            <button
+                                className='modal-btn modal-btn-primary'
+                                onClick={handleAddToCart}
+                            >
+                                <svg
+                                    width='16'
+                                    height='16'
+                                    viewBox='0 0 24 24'
+                                    fill='currentColor'
+                                >
+                                    <path d='M7 4V2C7 1.45 7.45 1 8 1H16C16.55 1 17 1.45 17 2V4H20C20.55 4 21 4.45 21 5S20.55 6 20 6H19V19C19 20.1 18.1 21 17 21H7C5.9 21 5 20.1 5 19V6H4C3.45 6 3 5.55 3 5S3.45 4 4 4H7ZM9 3V4H15V3H9ZM7 6V19H17V6H7Z' />
+                                </svg>
+                                Add to Cart
+                            </button>
+                        ) : (
+                            <button className='modal-btn modal-btn-secondary' disabled>
+                                Out of Stock
+                            </button>
+                        )} */}
 					</div>
 				</div>
 			</div>
