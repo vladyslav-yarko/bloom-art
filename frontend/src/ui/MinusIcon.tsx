c97ccbb40eb7d44f1980795e9b34045a458def6d
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-18 20:17:41 +0200

    feat: add MinusIcon

---
+++ frontend/src/ui/MinusIcon.tsx
@@ -0,0 +1,26 @@
+import { SVGProps } from "react"
+
+
+export default function MinusIcon(props: SVGProps<SVGSVGElement>) {
+	return (
+		<div className="text-red-500">
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='100%'
+				height='100%'
+				viewBox='0 0 26 26'
+				{...props}
+			>
+				{/* Icon from Pepicons Pop! by CyCraft - https://github.com/CyCraft/pepicons/blob/dev/LICENSE */}
+				<g fill='currentColor'>
+					<path d='M8 14a1 1 0 1 1 0-2h10a1 1 0 1 1 0 2z' />
+					<path
+						fillRule='evenodd'
+						d='M13 24c6.075 0 11-4.925 11-11S19.075 2 13 2S2 6.925 2 13s4.925 11 11 11m0 2c7.18 0 13-5.82 13-13S20.18 0 13 0S0 5.82 0 13s5.82 13 13 13'
+						clipRule='evenodd'
+					/>
+				</g>
+			</svg>
+		</div>
+	)
+}
