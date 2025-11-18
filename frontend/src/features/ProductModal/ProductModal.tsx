43ba7070a803fcbf4820adcb1526467a793618ed
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-18 15:37:30 +0200

    feat: add add to cart button for modal

--- frontend/src/features/ProductModal/ProductModal.tsx
+++ frontend/src/features/ProductModal/ProductModal.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { HomeContext } from "@/context/HomeContext";
 import ImageComponent from "@/ui/ImageComponent";
 import CloseIcon from "@/ui/CloseIcon";
+import ButtonPrimary from "@/ui/ButtonPrimary";
 
 
 export default function ProductModal() {
@@ -33,13 +34,18 @@ export default function ProductModal() {
     }
 
     return (
-			<div className='fixed inset-0 z-40 flex items-center justify-center bg-black/50 backdrop-blur-sm' onClick={onClose}>
-				<div className='grid grid-cols-1 justify-items-center gap-y-3 bg-background dark:bg-background-dark p-7 rounded-2xl
-				shadow transition hover:shadow hover:shadow-accent dark:hover:shadow-accent-dark' onClick={e => e.stopPropagation()}>
-					<div className="w-8 md:w-12 lg:w-14 mb-4 transition hover:rotate-90">
-						<CloseIcon onClick={onClose}/>
+			<div
+				className='fixed inset-0 z-40 flex items-center justify-center bg-black/50 backdrop-blur-sm'
+				onClick={onClose}
+			>
+				<div
+					className='grid grid-cols-1 justify-items-center gap-y-3 bg-background dark:bg-background-dark p-7 rounded-2xl
+				shadow transition hover:shadow hover:shadow-accent dark:hover:shadow-accent-dark mt-4'
+					onClick={e => e.stopPropagation()}
+				>
+					<div className='w-8 md:w-12 lg:w-14 mb-4 transition hover:rotate-90'>
+						<CloseIcon onClick={onClose} />
 					</div>
-					
 
 					<ImageComponent
 						src={`data:image/jpeg;base64,${productBase64}`}
@@ -47,28 +53,19 @@ export default function ProductModal() {
 						className='relative w-24 md:w-32 lg:w-40 h-24 md:h-32 lg:h-40 overflow-hidden rounded-lg'
 					/>
 
-
 					<h1 className='modal-title'>{selectedProduct?.title}</h1>
 					<h2 className='modal-price'>
 						{selectedProduct?.price} {selectedProduct?.currency}
 					</h2>
 
-					{selectedProduct?.description && (
-						<p>
-							{selectedProduct.description}
-						</p>
-					)}
+					{selectedProduct?.description && <p>{selectedProduct.description}</p>}
 
 					{selectedProduct?.available >= 1 ? (
-						<h2>
-							✅ Available
-						</h2>
+						<h2>✅ Available</h2>
 					) : (
-						<h2>
-							❌ Not Available
-						</h2>
+						<h2>❌ Not Available</h2>
 					)}
-						
+
 					{/* {product.available >= 1 ? (
 						<button
 							className='modal-btn modal-btn-primary'
@@ -89,6 +86,15 @@ export default function ProductModal() {
 							Out of Stock
 						</button>
 					)} */}
+
+					{selectedProduct.available >= 1 ? (
+						<div className="w-30 md:w-35 lg:w-40">
+							<ButtonPrimary>
+								{/* <LinkComponent title="Add to cart" link="/cart"/> */}
+								<h2>Add to cart</h2>
+							</ButtonPrimary>
+						</div>
+					) : null}
 				</div>
 			</div>
 		)
