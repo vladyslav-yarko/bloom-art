fb790f315c8beb739e692a0edf7d5ebd4f30bf79
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-19 17:47:37 +0200

    feat: add suspense for home cards

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
+import { Suspense } from 'react'
 import { getTranslations } from 'next-intl/server'
 
 import HomeProductCard from "@/features/HomeProductCard"
 import ProductModal from "@/features/ProductModal/ProductModal"
 import { Item } from "@/types/home"
+import LoadingState from '@/ui/LoadingState'
 
 
 const fetchPerfumes = async () => {
@@ -18,22 +20,30 @@ export default async function HomePage() {
     const perfumes = await fetchPerfumes()
 
     return (
-        <div className="shadow transition hover:shadow hover:shadow-accent dark:hover:shadow-accent-dark rounded-lg bg-card dark:bg-card-dark">
-            <ProductModal/>
-            <div className="flex flex-col text-center mb-8 p-4">
-                <h1>✨ ArtStudia ✨</h1>
-                <p>{t("titleBar.description")}</p>
-            </div>
-            {perfumes.length > 0 && (
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-2 md:p-4 lg:p-8 justify-items-center gap-y-10">
-                    {perfumes.map((element: Item, index: number): React.ReactNode => (
-                        <HomeProductCard 
-                            item={element} 
-                            key={index}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
-    )
+			<div className='shadow transition hover:shadow hover:shadow-accent dark:hover:shadow-accent-dark rounded-lg bg-card dark:bg-card-dark'>
+				<ProductModal />
+				<div className='flex flex-col text-center mb-8 p-4'>
+					<h1>✨ ArtStudia ✨</h1>
+					<p>{t('titleBar.description')}</p>
+				</div>
+				{perfumes.length > 0 && (
+					<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-2 md:p-4 lg:p-8 justify-items-center gap-y-10'>
+						{perfumes.map(
+							(element: Item, index: number): React.ReactNode => (
+								<Suspense
+									key={index}
+									fallback={
+										<div className='loadingIconCard'>
+											<LoadingState />
+										</div>
+									}
+								>
+									<HomeProductCard item={element} />
+								</Suspense>
+							)
+						)}
+					</div>
+				)}
+			</div>
+		)
 }
