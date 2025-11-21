4b8a113b2278d6f721ec679f2aeccd9d73c5bae1
vladyslav-yarko <vladislavv44444@gmail.com>
2025-11-22 00:47:57 +0200

    feat: add OrderItem & suspenses

--- frontend/src/features/OrderForm/OrderForm.tsx
+++ frontend/src/features/OrderForm/OrderForm.tsx
@@ -1,16 +1,34 @@
 import Form from 'next/form'
+import { Suspense } from 'react'
 
 import OrderContact from "./features/OrderContact"
 import OrderShipping from "./features/OrderShipping"
 import makeOrder from './server/makeOrder'
+import LoadingState from '@/ui/LoadingState'
 
 
 export default function OrderForm() {
 
     return (
-        <Form action={makeOrder} className="orderForm">
-            <OrderContact />
-            <OrderShipping />
-        </Form>
-    )
+			<Form action={makeOrder} className='orderForm'>
+				<Suspense
+					fallback={
+						<div className='loading'>
+							<LoadingState />
+						</div>
+					}
+				>
+					<OrderContact />
+				</Suspense>
+				<Suspense
+					fallback={
+						<div className='loading'>
+							<LoadingState />
+						</div>
+					}
+				>
+					<OrderShipping />
+				</Suspense>
+			</Form>
+		)
 }
--- frontend/src/features/OrderItem/OrderItem.tsx
+++ frontend/src/features/OrderItem/OrderItem.tsx
@@ -1,8 +1,36 @@
-export default function OrderItem() {
+import { Suspense, useContext } from 'react'
+
+import ImageComponent from '@/ui/ImageComponent'
+import LoadingState from '@/ui/LoadingState'
+import { OrderContext } from '@/context/OrderContext'
+import { toBase64 } from '@/lib/img'
+import SubmitButton from '@/ui/SubmitButton'
+
+
+function OrderItem() {
+    const {
+			item
+		} = useContext(OrderContext)!
+    const image = toBase64(item!.picture)
 
     return (
-        <div className="orderItem">
-
-        </div>
-    )
+			<div className='orderItem'>
+				<Suspense fallback={
+                    <div className='loadingIconCard'>
+                        <LoadingState />
+                    </div>}>
+					<ImageComponent src={image} alt='perfume' />
+					<h1>{item?.title}</h1>
+					<h2>{`${item?.price}${item?.currency}`}</h2>
+                    <div>
+                        <SubmitButton>
+                            <h2>Make order</h2>
+                        </SubmitButton>
+                    </div>
+				</Suspense>
+			</div>
+		)
 }
+
+
+export default OrderItem
